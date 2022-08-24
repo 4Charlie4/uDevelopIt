8af54cca697f729bf89d88b734b989f3fdd69026
Charles Duran <62945929+4Charlie4@users.noreply.github.com>
2022-08-24 14:29:11 -0400

    CRUD Routes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const { createTestScheduler } = require("jest");
 const mysql = require("mysql2");
+const inputCheck = require("./utils/inputCheck");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -21,37 +23,85 @@ const db = mysql.createConnection(
   console.log("Connected to the election database.")
 );
 
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//   console.log(rows);
-// });
+app.get("/api/candidates", (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "Success",
+      data: rows,
+    });
+  });
+});
 
 //query's for one candidate
-// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(row);
-// });
+app.get("/api/candidate/:id", (req, res) => {
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "Success",
+      data: row,
+    });
+  });
+});
 
 //Delete a candidate (use place holder to avoid SQL injection)
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
+app.delete("/api/candidate/:id", (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: "Candidate not found. Try again.",
+      });
+    } else {
+      res.json({
+        message: "Deleted",
+        changes: result.affectedRows,
+        id: req.params.id,
+      });
+    }
+  });
+});
 
 //Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//         VALUES(?,?,?,?)`;
-// const params = [1, "Ronald", "Firbank", 1];
+app.post("/api/candidate", ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    "first_name",
+    "last_name",
+    "industry_connected"
+  );
+  if (errors) {
+    res.status(400).json({ errors: errors });
+    return;
+  }
 
-// db.query(sql, params, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+  VALUES(?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "Created",
+      data: body,
+    });
+  });
+});
 
 //default response for request that fail/not found(Catchall route) always place at the bottom
 app.use((req, res) => {
