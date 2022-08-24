f766864fdb195c96e8a01d803d0efa8487b6d3f5
Charles Duran <62945929+4Charlie4@users.noreply.github.com>
2022-08-24 12:54:02 -0400

    connected server to database

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,12 +7,24 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connects to Database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+
+    user: "root",
+
+    password: "rcP9b1Lw3GnkX&duvF",
+
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
 
 //default response for request that fail/not found(Catchall route) always place at the bottom
 app.use((req, res) => {
   res.status(404).end();
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
